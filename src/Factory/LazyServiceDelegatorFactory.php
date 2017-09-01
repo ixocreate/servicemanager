a6ac051c2fbb7a787f3dc8280046dedb9908e690
Thomas Payer <1170097+thomaspayer@users.noreply.github.com>
2017-09-01 16:50:44 +0200

    added (non-functional) draft factories

---
+++ src/Factory/LazyServiceDelegatorFactory.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * kiwi-suite/servicemanager (https://github.com/kiwi-suite/servicemanager)
+ *
+ * @package kiwi-suite/servicemanager
+ * @see https://github.com/kiwi-suite/servicemanager
+ * @copyright Copyright (c) 2010 - 2017 kiwi suite GmbH
+ * @license MIT License
+ */
+
+declare(strict_types=1);
+namespace KiwiSuite\ServiceManager\Factory;
+
+use KiwiSuite\ServiceManager\DelegatorFactoryInterface;
+use Psr\Container\ContainerInterface;
+
+final class LazyServiceDelegatorFactory implements DelegatorFactoryInterface
+{
+    public function __invoke(ContainerInterface $container, $name, callable $callback, array $options = null)
+    {
+    }
+}
