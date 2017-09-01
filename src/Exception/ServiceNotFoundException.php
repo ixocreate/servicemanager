dfe938fbc044159579a4bededebd24939b2eb4ad
Thomas Payer <1170097+thomaspayer@users.noreply.github.com>
2017-09-01 16:50:28 +0200

    added default exceptions

---
+++ src/Exception/ServiceNotFoundException.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * kiwi-suite/servicemanager (https://github.com/kiwi-suite/servicemanager)
+ *
+ * @package kiwi-suite/servicemanager
+ * @see https://github.com/kiwi-suite/servicemanager
+ * @copyright Copyright (c) 2010 - 2017 kiwi suite GmbH
+ * @license MIT License
+ */
+
+declare(strict_types=1);
+namespace KiwiSuite\ServiceManager\Exception;
+
+use Psr\Container\NotFoundExceptionInterface;
+
+class ServiceNotFoundException extends \InvalidArgumentException implements NotFoundExceptionInterface
+{
+}
