5c8af0e5c96959278ac149c6a45af9137432231d
Thomas Payer <1170097+thomaspayer@users.noreply.github.com>
2017-12-12 12:10:41 +0100

    CS

--- tests/misc/ComplexObject.php
+++ tests/misc/ComplexObject.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ * kiwi-suite/servicemanager (https://github.com/kiwi-suite/servicemanager)
+ *
+ * @package kiwi-suite/servicemanager
+ * @see https://github.com/kiwi-suite/servicemanager
+ * @copyright Copyright (c) 2010 - 2017 kiwi suite GmbH
+ * @license MIT License
+ */
+
+declare(strict_types=1);
 namespace KiwiSuiteMisc\ServiceManager;
 
 class ComplexObject
@@ -11,6 +21,5 @@ class ComplexObject
         \DateTimeInterface $value3,
         string $value4
     ) {
-
     }
 }
