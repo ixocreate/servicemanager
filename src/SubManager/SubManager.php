b8eca5363a4c5c8d7ad138b31cc7ae322905b1db
Lucas Seinfeld <l.seinfeld@ixolit.com>
2019-06-12 12:23:31 +0200

    fix cs

--- src/SubManager/SubManager.php
+++ src/SubManager/SubManager.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * @link https://github.com/ixocreate
+ * @copyright IXOLIT GmbH
+ * @license MIT License
+ */
 
 declare(strict_types=1);
 
@@ -11,5 +16,4 @@ namespace Ixocreate\ServiceManager\SubManager;
  */
 class SubManager extends AbstractSubManager
 {
-
 }
